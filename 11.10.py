2cc55123923af677ee06e00622bf33e482728174
Andreeapripa <70636457+Andreeapripa@users.noreply.github.com>
2021-10-12 21:35:17 +0300

    Add files via upload

---
+++ 11.10.py
@@ -0,0 +1,5 @@
+nume=str(input('Ce nume ai?'))
+prenume=str(input('Ce prenume ai?'))
+meseria=str(input('Unde doresti sa lucrezi?'))
+print('Te numesti', nume , prenume)
+print('Doresti sa devii', meseria)
